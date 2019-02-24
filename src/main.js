47b7dddf3be551199a6dd58f4cfd5d300ec115b0
Ksenia <filippovaki@yandex.ru>
2019-02-24 22:28:26 +0300

    Homework #1

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import {createFilter} from './create-filter';
+import {createTripPoint} from './create-trip-point';
+
+const filtersData = [
+  {name: `Everything`, isChecked: true},
+  {name: `Future`, isChecked: false},
+  {name: `Past`, isChecked: false}
+];
+
+const renderTripPoints = (count) => {
+  const tripPointsContainer = document.querySelector(`.trip-day__items`);
+  tripPointsContainer.innerHTML = ``;
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < count; i++) {
+    fragment.appendChild(createTripPoint());
+    tripPointsContainer.appendChild(fragment);
+  }
+};
+
+const renderFilters = (data) => {
+  const filtersContainer = document.querySelector(`.trip-filter`);
+  const fragment = document.createDocumentFragment();
+  data.forEach((filter) => fragment.appendChild(createFilter(filter, renderTripPoints)));
+  filtersContainer.appendChild(fragment);
+};
+
+renderFilters(filtersData);
+renderTripPoints(4);
