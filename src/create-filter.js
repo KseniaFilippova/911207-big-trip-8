f6d8b3dd799a2d8ddbaf02ab6be3fcb28823523d
Ksenia <filippovaki@yandex.ru>
2019-02-26 14:26:23 +0300

    Homework correction

--- src/create-filter.js
+++ src/create-filter.js
@@ -1,24 +1,11 @@
-const MAX_TRIP_POINTS_COUNT = 10;
+const createFilter = (data) => {
+  const checked = data.isChecked ? `checked` : ``;
+  const id = data.name.toLowerCase();
 
-const createRandomIntegerNumber = (minNumber, maxNumber) => Math.floor(minNumber + Math.random() * (maxNumber + 1 - minNumber));
-
-const createElement = (template) => {
-  const templateElement = document.createElement(`template`);
-  templateElement.innerHTML = template;
-  return templateElement.content;
-};
-
-const createFilter = (filterData, renderTripPointsDelegate) => {
-  const id = filterData.name.toLowerCase();
-  const template = `
-    <input type="radio" id="filter-${id}" name="filter" value="${id}" checked="${filterData.isChecked}">
-    <label class="trip-filter__item" for="filter-${id}">${filterData.name}</label>
+  return `
+    <input type="radio" id="filter-${id}" name="filter" value="${id}" ${checked}>
+    <label class="trip-filter__item" for="filter-${id}">${data.name}</label>
   `;
-  const element = createElement(template);
-  const input = element.querySelector(`input`);
-  input.addEventListener(`change`, () => renderTripPointsDelegate(createRandomIntegerNumber(0, MAX_TRIP_POINTS_COUNT)));
-
-  return element;
 };
 
 export {createFilter};
--- src/main.js
+++ src/main.js
@@ -1,31 +1,38 @@
 import {createFilter} from './create-filter';
 import {createTripPoint} from './create-trip-point';
 
+const MAX_TRIP_POINTS_COUNT = 10;
 const DEFAULT_TRIP_POINTS_COUNT = 7;
+
+const filtersContainer = document.querySelector(`.trip-filter`);
+const tripPointsContainer = document.querySelector(`.trip-day__items`);
 
 const filtersData = [
   {name: `Everything`, isChecked: true},
-  {name: `Future`, isChecked: false},
-  {name: `Past`, isChecked: false}
+  {name: `Future`},
+  {name: `Past`}
 ];
 
-const renderTripPoints = (count) => {
-  const tripPointsContainer = document.querySelector(`.trip-day__items`);
-  tripPointsContainer.innerHTML = ``;
-
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < count; i++) {
-    fragment.appendChild(createTripPoint());
-    tripPointsContainer.appendChild(fragment);
-  }
-};
+const createRandomIntegerNumber = (minNumber, maxNumber) => Math.floor(minNumber + Math.random() * (maxNumber + 1 - minNumber));
 
 const renderFilters = (data) => {
-  const filtersContainer = document.querySelector(`.trip-filter`);
-  const fragment = document.createDocumentFragment();
-  data.forEach((filter) => fragment.appendChild(createFilter(filter, renderTripPoints)));
-  filtersContainer.appendChild(fragment);
+  const filters = data.map(createFilter);
+  filtersContainer.innerHTML = filters.join(``);
+};
+
+const renderTripPoints = (count) => {
+  const tripPoints = new Array(count);
+  for (let i = 0; i < count; i++) {
+    tripPoints[i] = createTripPoint();
+  }
+  tripPointsContainer.innerHTML = tripPoints.join(``);
+};
+
+const onFiltersChange = () => {
+  renderTripPoints(createRandomIntegerNumber(0, MAX_TRIP_POINTS_COUNT));
 };
 
 renderFilters(filtersData);
 renderTripPoints(DEFAULT_TRIP_POINTS_COUNT);
+
+filtersContainer.addEventListener(`change`, onFiltersChange);
