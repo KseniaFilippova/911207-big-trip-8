f082bb135af3222c40624e9b3efa5142bb944c52
Ksenia <filippovaki@yandex.ru>
2019-03-22 15:24:37 +0300

    Homework module6

---
+++ src/trip-types-data.js
@@ -0,0 +1,28 @@
+const tripTypesData = {
+  'taxi': {
+    icon: `🚕`,
+    action: `Taxi ride to`,
+  },
+  'bus': {
+    icon: `🚌`,
+    action: `Bus ride to`,
+  },
+  'train': {
+    icon: `🚂`,
+    action: `Train ride to`,
+  },
+  'flight': {
+    icon: `✈`,
+    action: `Flight to`,
+  },
+  'check-in': {
+    icon: `🏨`,
+    action: `Check in`,
+  },
+  'sightseeing': {
+    icon: `🏛`,
+    action: `Sightseeing`,
+  },
+};
+
+export {tripTypesData};
