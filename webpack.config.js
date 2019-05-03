e8d70ae29580e9614d1fbb782d8fcb87bd3c6f32
Ksenia <filippovaki@yandex.ru>
2019-05-03 13:46:26 +0300

    Deploy configuration set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,42 @@
 const path = require(`path`);
 
 module.exports = {
-  mode: `development`,
-  entry: `./src/main.js`,
+  entry: [
+    `./src/main.js`
+  ],
   output: {
-    filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+    path: path.join(__dirname, `public`),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
   },
   devtool: `source-map`,
   devServer: {
